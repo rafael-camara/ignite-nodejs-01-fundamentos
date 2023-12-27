5f0e38e14750ab150eb788742cf044f650255dc3
Rafael Câmara <rafaelcamara.377@gmail.com>
2023-12-27 18:49:07 -0300

    Creating JSON middleware

--- src/server.js
+++ src/server.js
@@ -1,49 +1,15 @@
 import http from 'node:http'
-
-// - Criar usuários
-// - Listar usuários
-// - Edição de usuários
-// - Deletar usuários
-
-// CRUD - Create, Read, Update, Delete
-
-// - HTTP
-// - Métodos HTTP: GET, POST, PUT, PATCH, DELETE
-// - URL
-// - Status Code: 200, 201, 404, 500
-
-// GET => Buscar um recurso no servidor
-// POST => Criar um novo recurso no servidor
-// PUT => Atualizar um recurso existente no servidor
-// PATCH => Atualizar uma informação de um recurso existente no servidor
-// DELETE => Deletar um recurso do servidor
-
-// HTTP Status Codes => https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-Type', 'application/json')
       .end(JSON.stringify(users))
   }
 
@@ -56,7 +22,7 @@ const server = http.createServer(async (req, res) => {
       email
     })
 
-    return res.writeHead(201).end('Criando um novo usuário')
+    return res.writeHead(201).end()
   }
 
   return res.writeHead(404).end()
