0037ab36d062b0af965199083890cf2272ba3e13
Rafael Câmara <rafaelcamara.377@gmail.com>
2023-12-27 18:07:29 -0300

    Route, create, list (method HTTP)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,36 @@
 import http from 'node:http'
 
+// - Criar usuários
+// - Listar usuários
+// - Edição de usuários
+// - Deletar usuários
+
+// CRUD - Create, Read, Update, Delete
+
+// - HTTP
+// - Métodos HTTP: GET, POST, PUT, PATCH, DELETE
+// - URL
+// - Status Code: 200, 201, 404, 500
+
+// GET => Buscar um recurso no servidor
+// POST => Criar um novo recurso no servidor
+// PUT => Atualizar um recurso existente no servidor
+// PATCH => Atualizar uma informação de um recurso existente no servidor
+// DELETE => Deletar um recurso do servidor
+
+// HTTP Status Codes => https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criando um novo usuário')
+  }
+
   return res.end('Hello World')
 })
 
