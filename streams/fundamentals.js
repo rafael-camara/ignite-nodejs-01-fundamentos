cff44e2bea11039b40f86e4cdd644537737b2ed0
Rafael Câmara <rafaelcamara.377@gmail.com>
2023-12-27 18:21:51 -0300

    Creating reading stream

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,13 +1,22 @@
-// Netflix & Spotify
+import { Readable } from 'node:stream'
 
-// Importação de clientes via CSV (Excel)
-// 1gb - 1.000.000
-// POST /upload import.csv
+class OneToHundredStream extends Readable {
+  index = 1
 
-// 10mb/s - 100s
+  _read() {
+    const i = this.index++
 
-// 100s -> Inserção no banco de dados
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
 
-// 10mb/s -> 10.000
+        this.push(buf)
+      }
+    }, 1000);
+  }
+}
 
-// Readable Streams / Writable Streams
+new OneToHundredStream()
+  .pipe(process.stdout)
