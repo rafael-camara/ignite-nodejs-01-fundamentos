95596696cea43bcbf23d93c264c5fdf091cffa00
Rafael Câmara <rafaelcamara.377@gmail.com>
2023-12-27 18:13:48 -0300

    Saving users in memory (Headers)

--- src/server.js
+++ src/server.js
@@ -20,14 +20,28 @@ import http from 'node:http'
 
 // HTTP Status Codes => https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
 
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
     return res.end('Criando um novo usuário')
   }
 
